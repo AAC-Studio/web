4bdaf4df22666de83041646d34c71f5f9905e65c
D01-aacstudio <d01.aacstudio@gmail.com>
2021-05-15 12:55:22 +0530

    service add  slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,8 @@
 
-
  $(document).ready(function(){
+
+   
+
   $('.count').each(function () {
          $(this).prop('Counter',0).animate({
         Counter: $(this).text()
@@ -12,6 +14,28 @@
         }
     });
   });
+  $('.slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots:false,
+    navText: [
+      "<i class='fa fa-caret-left'></i>",
+      "<i class='fa fa-caret-right'></i>"
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      600: {
+        items: 2
+      }
+      
+    }
+  })
+  
 })
 $(window).scroll(function() {
   if ($(this).scrollTop() > 50 ) {
