4982e21ba71cfd2e014b71e11b6b45494a4aa700
D01-aacstudio <d01.aacstudio@gmail.com>
2021-06-04 18:35:48 +0530

    blog

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -70,6 +70,28 @@
   })
   
 })
+$('.latest-bolg-slider').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots:false,
+  navText: [
+    "<i class='fa fa-angle-left'></i>",
+    "<i class='fa fa-angle-right'></i>"
+  ],
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    }
+
+  }
+})
+
 $(window).scroll(function(){
   if ($(window).scrollTop() >= 30) {
       $('header').addClass('sticky');
